99aa38ff9b8995afb69c69964005a3fe01f5c166
n9te9 <ai.kanaria.ai@gmail.com>
2026-02-24 15:44:46 +0900

    improve: planner

--- _example/ec/inventory/main.go
+++ _example/ec/inventory/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -11,6 +15,27 @@ import (
 )
 
 const defaultPort = "8080"
+
+func accessLog(service string, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		body, _ := io.ReadAll(r.Body)
+		r.Body = io.NopCloser(bytes.NewReader(body))
+		operation := "-"
+		body400 := string(body)
+		if len(body400) > 400 {
+			body400 = body400[:400]
+		}
+		if strings.Contains(body400, "_entities") {
+			operation = "_entities"
+		} else if strings.Contains(body400, "query") || strings.Contains(body400, "mutation") {
+			operation = "rootQuery"
+		}
+		next.ServeHTTP(w, r)
+		log.Printf("[ACCESS] service=%s op=%s path=%s remote=%s dur=%s",
+			service, operation, r.URL.Path, r.RemoteAddr, time.Since(start))
+	})
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -20,9 +45,10 @@ func main() {
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	mux := http.NewServeMux()
+	mux.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	mux.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, accessLog("inventory", mux)))
 }
